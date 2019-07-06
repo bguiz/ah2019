adecdc99f7ca94ca06b562b44e7d9c23d3324b31
bguiz <github@bguiz.com>
2019-07-06 21:51:53 +0800

    feat: implement getResults (aggergated) API

--- server/server.js
+++ server/server.js
@@ -76,5 +76,65 @@ server.post('/getSurvey', async (req, res) => {
 
 });
 
+server.post('/getResults', async (req, res) => {
+  console.log('/getResults', req.body);
+
+  try {
+    const {
+      surveyId,
+      numQuestions,
+    } = req.body;
+    const address = await web3.getAccount();
+
+    const ipfsHash = await web3.contract.methods
+      .surveys(surveyId)
+      .call({
+        from: address,
+      });
+
+    if (!ipfsHash) {
+      res.status(404).send(`Survey does not exist: ${surveyId}`);
+      return;
+    }
+
+    const survey = await ipfsReadObject(ipfsHash);
+    console.log(survey);
+
+    const results = await web3.contract.methods
+      .aggregateSurvey(surveyId, numQuestions)
+      .call({
+        from: address,
+      });
+    const numbers = results.map((bn) => bn.toNumber());
+    const questions = [...survey.questions];
+    for (let i = 0; i < numQuestions; i++) {
+      const answerCounts = [
+        numbers[i * 4 + 0],
+        numbers[i * 4 + 1],
+        numbers[i * 4 + 2],
+        numbers[i * 4 + 3],
+      ];
+      const question = {
+        ...questions[i],
+        answerCounts,
+      };
+      questions[i] = question;
+    }
+
+    const updatedSurvey = {
+      ...survey,
+      questions,
+    };
+
+    res.send({
+      survey: updatedSurvey,
+    });
+  } catch (ex) {
+    res.status(500).send(ex.toString());
+    console.error(ex);
+  }
+
+});
+
 server.listen(port);
 console.log(`Listening on http://127.0.0.1:${port}`);
